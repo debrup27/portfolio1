5ab64cca6a101218bbe343c54140e5ede6807c1b
debrup27 <debrupsenguptabusiness@gmail.com>
2024-12-07 07:36:37 +0530

    Initial creation along with bug fixes and other corrections, requires addition of bento box and redirecting bento box to website links, along with addition of CV.

---
+++ data/index.ts
@@ -0,0 +1,221 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+//WORK ON LINKS OF BENTO
+export const gridItems = [
+  {
+    id: 1,
+    title: "I work on crafting user-centric solutions, treating client collaboration and open-communication as a priority.",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/trophy.jpeg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm flexible with inter-time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I am constantly trying to improve and expand",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for Artificial Intelligence and Machine Learning.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently working on a Project for the Detection of Sudden Cloud Burst and Preventive Counter-Measures",
+    description: "The Insider's View",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-[10rem] md:w-80 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/output.png",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together or Hire me for a project?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Winner of Nirmiti 2024(Tier-I)",
+    des: "I was the Operations Manager/Leader of the winning team of Nirmiti 2024,A Startup Conclave.",
+    img: "/trophy.jpeg",
+    iconLists: ["/small-trophy.png"],
+    link: "/youtube.com/watch?v=Jh7fmU0TWIA&t=28968s",
+  },
+  {
+    id: 2,
+    title: "Brain Tumour Detection",
+    des: "A ML endeavour utilizing CNN to detect and classify the type of Brain Tumour with high accuracy.",
+    img: "/braincnn.png",
+    iconLists: ["/py.png","jupy.png","ml.avif"],
+    link: "/github.com/debrup27",
+  },
+  {
+    id: 3,
+    title: "Detection of Cloudbursts and Predictive Counteraction",
+    des: "Cloudbursts can often be intense and damaging, this project aims to detect the cloudbursts along with its intensity from satellite data, enabling the authorities to take preventive measures well ahead of time.",
+    img: "/output.png",
+    iconLists: ["/py.png","jupy.png","ml.avif"],
+    link: "/github.com/debrup27",
+  },
+  {
+    //UPDATE LATER
+    id: 4,
+    title: "In Progress",
+    des: "....",
+    img: "/p4.svg",
+    iconLists: ["/re.svg"],
+    link: "/github.com/debrup27",
+  },
+];
+
+export const testimonials = [
+  {
+        quote:
+      "Collaborating with Debrup was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Debrup's enthusiasm for every aspect of development truly stands out. If you're seeking out to elevate your projects, Debrup is the ideal partner.",
+    name: "Saikat Bandyopadhyay",
+    title: "Asst.Professor at Netaji Subhash Engineering College",
+  },
+  {
+    quote:
+      "Collaborating with Debrup was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Debrup's enthusiasm for every aspect of development truly stands out. If you're seeking out to elevate your projects, Debrup is the ideal partner.",
+    name: "Saikat Bandyopadhyay",
+    title: "Asst.Professor at Netaji Subhash Engineering College",
+  },
+  {
+    quote:
+      "Collaborating with Debrup was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Debrup's enthusiasm for every aspect of development truly stands out. If you're seeking out to elevate your projects, Debrup is the ideal partner.",
+    name: "Saikat Bandyopadhyay",
+    title: "Asst.Professor at Netaji Subhash Engineering College",
+  },
+  {
+    quote:
+      "Collaborating with Debrup was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Debrup's enthusiasm for every aspect of development truly stands out. If you're seeking out to elevate your projects, Debrup is the ideal partner.",
+    name: "Saikat Bandyopadhyay",
+    title: "Asst.Professor at Netaji Subhash Engineering College",
+  },
+  {
+    quote:
+      "Collaborating with Debrup was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Debrup's enthusiasm for every aspect of development truly stands out. If you're seeking out to elevate your projects, Debrup is the ideal partner.",
+    name: "Saikat Bandyopadhyay",
+    title: "Asst.Professor at Netaji Subhash Engineering College",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Operations Manager/Leader",
+    desc: "Currently the Team Lead of winners of Nirmiti 2024(Tier-I) and also the Lead of Team Basilisk.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Aspiring App Developer",
+    desc: "Designed and developed mobile applications for Android platforms using React Native.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Lead of a project on GPS Denied Navigation",
+    desc: "Led the development of a project on GPS Denied Navigation during SIH,and worked on the Machine Learning aspects.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Machine Learning Engineer",
+    desc: "Working on multiple real-life scenario projects aimed at making an impact on the human society.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link:"https://github.com/debrup27",
+  },
+  {
+    id: 2,
+    img: "/link.svg",
+    link: "https://www.linkedin.com/in/debrup-sengupta-36b72825b/",
+  },
+  {
+     id: 3,
+     img: "/cv.png",
+    // link: "https://www.linkedin.com/in/debrup-sengupta-36b72825b/", //update later
+ },
+];
