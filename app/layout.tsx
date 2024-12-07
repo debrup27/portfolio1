0b12295c458ac33695133051f41eeec15cb57133
debrup27 <debrupsenguptabusiness@gmail.com>
2024-12-07 12:44:17 +0530

    Changed Logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="/jsm-logo.png" sizes="any" />
+        <link rel="icon" href="/d.jpeg" sizes="any" />
       </head>
       <body className={inter.className}>
         <ThemeProvider
